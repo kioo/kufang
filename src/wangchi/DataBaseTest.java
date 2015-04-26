61d7191f0daa387925379106d4998941d252897c
kioo <f2114198@163.com>
2015-04-26 19:21:14 +0800

    解除查询语句的耦合

--- src/wangchi/DataBaseTest.java
+++ src/wangchi/DataBaseTest.java
@@ -2,27 +2,53 @@ package wangchi;
 
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Vector;
 
 import com.mysql.jdbc.Connection;
 import com.mysql.jdbc.PreparedStatement;
+
 //数据库页面,change
 public class DataBaseTest {
 	PreparedStatement ps = null;
 	Connection ct = null;
 	ResultSet rs = null;
+	Vector<Vector> jilu;// 创建存放获取数据的列表
 
-	public void Query() {
+	public DataBaseTest() {
+		// 初始化连接数据库的驱动连接数据库
 		try {
-			String sql = "select code2 from gangwei_cailiao";
 			Class.forName("com.mysql.jdbc.Driver");
 			ct = (Connection) DriverManager
 					.getConnection("jdbc:mysql://localhost:3306/shangpin?user=root&password=5218791kioo)&useUnicode=true&characterEncoding=UTF8");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	// 1.查询所有信息
+	public Vector<Vector> Query() {
+		jilu = new Vector<Vector>(); // 创建查询结果存放的容器
+		try {
+			String sql = "select * from cailiaozhouqi";
 			ps = (PreparedStatement) ct.prepareStatement(sql);
 			rs = ps.executeQuery();
-			int i=0;
+
 			while (rs.next()) {
-				System.out.println(rs.getInt(1));
-				System.out.println(i++);
+				Vector hang = new Vector();
+				hang.add(rs.getInt(1));
+				hang.add(rs.getString(2));
+				hang.add(rs.getString(3));
+				hang.add(rs.getString(4));
+				hang.add(rs.getString(5));
+				hang.add(rs.getString(6));
+				hang.add(rs.getFloat(7));
+				hang.add(rs.getString(8));
+				hang.add(rs.getString(9));
+				hang.add(rs.getDate(10));
+				jilu.add(hang);
 			}
 		} catch (Exception e) {
 		} finally {
@@ -39,8 +65,18 @@ public class DataBaseTest {
 			} catch (Exception e) {
 			}
 		}
+		return jilu;
 	}
 
+	// 2.通过名称查询
+
+	// 3.通过字段查询
+
+	// 4.添加
+
+	// 5.修改
+
+	// 6.删除
 	public static void main(String[] args) {
 		DataBaseTest d = new DataBaseTest();
 		d.Query();
--- src/wangchi/InformationPage.java
+++ src/wangchi/InformationPage.java
@@ -1,13 +1,8 @@
 package wangchi;
 
-import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.util.Vector;
 
 import javax.swing.table.AbstractTableModel;
-
-import com.mysql.jdbc.Connection;
-import com.mysql.jdbc.PreparedStatement;
 
 public class InformationPage extends AbstractTableModel {
 
@@ -16,9 +11,7 @@ public class InformationPage extends AbstractTableModel {
 	 */
 	Vector<String> ziduan;
 	Vector<Vector>jilu;//创建存放获取数据的列表
-	PreparedStatement ps = null;
-	Connection ct = null;
-	ResultSet rs = null;
+	DataBaseTest db;
 	@Override
 	public int getColumnCount() {
 		// 获取列的总数
@@ -62,46 +55,8 @@ public class InformationPage extends AbstractTableModel {
 		ziduan.add("使用周期");
 		ziduan.add("改变日期");
 		
+		db = new DataBaseTest();
 		jilu = new Vector<Vector>();
-		
-		//连接数据库
-		try{
-			Class.forName("com.mysql.jdbc.Driver");
-			ct = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/shangpin?user=root&password=5218791kioo)&useUnicode=true&characterEncoding=UTF8");
-			ps = (PreparedStatement) ct.prepareStatement(sql);
-			rs = ps.executeQuery();
-			
-			while(rs.next()){
-				Vector hang = new Vector();
-				hang.add(rs.getInt(1));
-				hang.add(rs.getString(2));
-				hang.add(rs.getString(3));
-				hang.add(rs.getString(4));
-				hang.add(rs.getString(5));
-				hang.add(rs.getString(6));
-				hang.add(rs.getFloat(7));
-				hang.add(rs.getString(8));
-				hang.add(rs.getString(9));
-				hang.add(rs.getDate(10));
-				jilu.add(hang);
-			}
-		}catch(Exception e){}
-		finally{
-			try {
-	    		if(rs!=null)
-				{
-					rs.close();
-				}
-	    		if(ps!=null)
-				{
-					ps.close();
-				}
-				if(ct!=null)
-				{
-					ct.close();
-					System.out.println("查询到的行数为:"+jilu.size());
-				}
-			} catch (Exception e){}
-		}
+		jilu = db.Query();
 	}
 }
